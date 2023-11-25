7565e6180a16aca28202a601bd003ef714ccd725
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-25 14:28:08 +0500

    All rules and resource added

---
+++ app/src/main/java/uz/jasurbekruzimov/translatorapp/MainActivity.java
@@ -0,0 +1,16 @@
+package uz.jasurbekruzimov.translatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
