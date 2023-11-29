8df2077273038d593dd89a5bc308f3f22bbd0e84
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-29 14:02:40 +0500

    gradle and themes is update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,11 +24,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-
         }
-
-
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,14 +34,15 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("com.google.firebase:firebase-core:21.1.1")
-    implementation("com.google.firebase:firebase-ml-natural-language:26.7.0")
-    implementation("com.google.firebase:firebase-ml-natural-language-language-id-model:20.0.9")
+    implementation("com.google.firebase:firebase-ml-natural-language:22.0.1")
+    implementation("com.google.firebase:firebase-ml-natural-language-language-id-model:20.0.8")
     implementation("com.google.firebase:firebase-ml-natural-language-translate-model:20.0.9")
     implementation("com.google.firebase:firebase-ml-vision:24.1.0")
+    implementation("com.google.android.gms:play-services-vision:20.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
-        classpath ("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
